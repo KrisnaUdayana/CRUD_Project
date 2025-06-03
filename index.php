344a9c0b014f5ba05bb886545c7a7b0fe169bd7a
KrisnaUdayana <krisnaudayana05@gmail.com>
2025-06-03 20:20:11 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "unudphone";
+
+    $koneksi = mysqli_connect($host, $user, $pass, $db);
+    if (!$koneksi) {
+        die ("Tidak terhubung ke database");
+    } else {
+        echo "Koneksi Berhasil";
+    }
