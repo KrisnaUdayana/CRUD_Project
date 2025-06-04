c591ff891c69618cd9720fbe289da4137e053737
KrisnaUdayana <krisnaudayana05@gmail.com>
2025-06-04 09:09:32 +0800

    make table data

--- index.php
+++ index.php
@@ -1,13 +1,87 @@
 <?php
+require "module-hp.php";
+require "koneksi.php";
+?>
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db   = "unudphone";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Handphone</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
 
-    $koneksi = mysqli_connect($host, $user, $pass, $db);
-    if (!$koneksi) {
-        die ("Tidak terhubung ke database");
-    } else {
-        echo "Koneksi Berhasil";
-    }
+<body>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col sm-6">
+                        <h1 class="m-0">Handphone</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <h3 class="card-title"><i class="fas fa-list fa-sm mr-1"></i>Data Handphone</h3>
+                        <a href="<?= $main_url ?>form-hp.php" class="mr-2 btn btn-sm btn-primary float-end"><i class="fas fa-plus fa-sm mr-1"></i>Tambah Handphone</a>
+                    </div>
+                    <div class="card-body table-responsive p-3">
+                        <table class="table table-hover text-nowrap" id="tblData">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">Id Barang</th>
+                                    <th class="text-center">Nama</th>
+                                    <th class="text-center">Merk</th>
+                                    <th class="text-center">Harga</th>
+                                    <th class="text-center">Stock</th>
+                                    <th class="text-center">Warna</th>
+                                    <th class="text-center">Storage</th>
+                                    <th class="text-center">Ram</th>
+                                    <th style="width:10%;" class="text-center">Operasi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $no = 1;
+                                $handphone = getData("SELECT * FROM handphone ORDER BY LOWER(nama_handphone) ASC");
+                                foreach ($handphone as $hp) { ?>
+                                    <tr>
+                                        <td class="text-center"><?= $brg['id_barang'] ?></td>
+                                        <td class="text-center"><?= $brg['nama_barang'] ?></td>
+                                        <td class="text-center"><?= number_format($brg['harga_beli'], 0, ',', '.') ?></td>
+                                        <td class="text-center"><?= number_format($brg['harga_jual'], 0, ',', '.') ?></td>
+                                        <td class="text-center">
+                                            <a href="form-barang.php?id=<?= $brg['id_barang'] ?>&msg=editing" class="btn btn-warning btn-sm" title="edit barang"><i class="fas fa-pen"></i></a>
+                                            <a href="#" class="btn btn-danger btn-sm delete-btn"
+                                                data-url="?id=<?= $brg['id_barang'] ?>&msg=deleted"
+                                                title="hapus barang">
+                                                <i class="fas fa-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php
+
+                                }
+
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+    </div>
+
+
+
+
+</body>
+
+</html>
---
+++ module-hp.php
@@ -0,0 +1,73 @@
+<?php
+require_once 'koneksi.php';
+
+function getData($query = "SELECT * FROM handphone ORDER BY nama_handphone ASC")
+{
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    return $data;
+}
+
+
+function insert($data){
+    global $koneksi;
+
+    $id = mysqli_real_escape_string($koneksi, $data['kode']);
+    $nama = mysqli_real_escape_string($koneksi, $data['nama']);
+    $merk = mysqli_real_escape_string($koneksi, $data['merk']);
+    $harga = mysqli_real_escape_string($koneksi, $data['harga']);
+    $stock = mysqli_real_escape_string($koneksi, $data['stock']);
+    $warna = mysqli_real_escape_string($koneksi, $data['warna']);
+    $storage = mysqli_real_escape_string($koneksi, $data['storage']);
+    $ram = mysqli_real_escape_string($koneksi, $data['ram']);
+
+
+
+    $sqlHp = "INSERT INTO handphone VALUES ('$id', '$nama', '$merk', '$harga', '$stock', '$warna','$storage', '$ram')";
+    
+    mysqli_query($koneksi, $sqlHp);
+    return mysqli_affected_rows($koneksi);
+}
+
+function delete($id){
+    global $koneksi;
+
+    $sqlDel = "DELETE FROM handphone WHERE id_handphone = '$id'";
+    mysqli_query($koneksi, $sqlDel);
+
+    return mysqli_affected_rows($koneksi);
+
+}
+
+function update ($data){
+    global $koneksi;
+
+    $id = mysqli_real_escape_string($koneksi, $data['kode']);
+    $nama = mysqli_real_escape_string($koneksi, $data['nama']);
+    $merk = mysqli_real_escape_string($koneksi, $data['merk']);
+    $harga = mysqli_real_escape_string($koneksi, $data['harga']);
+    $stock = mysqli_real_escape_string($koneksi, $data['stock']);
+    $warna = mysqli_real_escape_string($koneksi, $data['warna']);
+    $storage = mysqli_real_escape_string($koneksi, $data['storage']);
+    $ram = mysqli_real_escape_string($koneksi, $data['ram']);
+
+    mysqli_query($koneksi, "UPDATE handphone SET
+                            nama_handphone = '$nama',
+                            merk_handphone = '$merk',
+                            harga = '$harga',
+                            stock = '$stock',
+                            warna = '$warna',
+                            storage = '$storage',
+                            ram = '$ram'
+                            WHERE id_handphone = '$id'
+    ");
+    return mysqli_affected_rows($koneksi);
+
+}
+
+
+?>
